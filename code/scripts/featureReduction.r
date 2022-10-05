30d1861e0db93d8b0e549e0104e81a80f0e9bc5a
mohanadarafe <marafe@live.com>
2022-10-05 14:38:18 -0400

    Add error handling in R feature reduction script

--- code/scripts/featureReduction.r
+++ code/scripts/featureReduction.r
@@ -1,5 +1,26 @@
 # Load library
-library(mRMRe)
+if (!require('mRMRe')) install.packages('mRMRe'); library('mRMRe')
+if (!require('crayon')) install.packages('crayon'); library('crayon')
+
+# Top number of features (user input)
+numberOfFeatures <- commandArgs(trailingOnly = TRUE)
+
+check.integer <- function(N){
+    !length(grep("[^[:digit:]]", as.character(N)))
+}
+
+if (length(numberOfFeatures) > 1) { 
+    cat(red("Error found! Too many arguments. Please supply 1 argument.\n"))
+    stop() 
+}
+if (length(numberOfFeatures) == 0) { 
+    cat(red("Error found! Missing argument. Please supply 1 argument.\n"))
+    stop()
+}
+if (check.integer(numberOfFeatures) != TRUE) {
+    cat(red("Error found! The argument must be an integer.\n"))
+    stop()
+}
 
 # Read CSV
 df <- read.csv("outputs/radiomics/features.csv", header = TRUE)
@@ -11,9 +32,9 @@ df[,ncol(df)] <- as.numeric(df[,ncol(df)])
 f_data <- mRMR.data(data = data.frame(df))
 
 # Perform classic mRMR
-solution <- mRMR.classic(data=f_data, target_indices=57, feature_count = 7)
+solution <- mRMR.classic(data=f_data, target_indices=ncol(df), feature_count = as.integer(numberOfFeatures))
 
 # Get index of top 7 features
 write.csv(solutions(solution), "outputs/radiomics/featureIndex.csv")
 
-print("Succesfully computed top features, index available in: outputs/radiomics/featureIndex.csv")
+cat(green("Succesfully computed top features, index available in: outputs/radiomics/featureIndex.csv"))
