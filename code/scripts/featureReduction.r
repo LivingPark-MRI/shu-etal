5949239bf51623a35ec72e08eb1fc2d523482002
mohanadarafe <marafe@live.com>
2023-01-31 21:51:24 -0500

    New notebook with entire replication & reproduction

--- code/scripts/featureReduction.r
+++ code/scripts/featureReduction.r
@@ -3,7 +3,9 @@ if (!require('mRMRe')) install.packages('mRMRe'); library('mRMRe')
 if (!require('crayon')) install.packages('crayon'); library('crayon')
 
 # Top number of features (user input)
-numberOfFeatures <- commandArgs(trailingOnly = TRUE)
+args <- commandArgs(trailingOnly = TRUE)
+numberOfFeatures = args[1]
+cohortType = args[2]
 
 check.integer <- function(N){
     !length(grep("[^[:digit:]]", as.character(N)))
@@ -23,10 +25,20 @@ if (check.integer(numberOfFeatures) != TRUE) {
 }
 
 # Read CSV
-df <- read.csv("outputs/radiomics/features.csv", header = TRUE)
+if (cohortType == "reference"){
+    df <- read.csv("outputs/radiomics/Reference Cohort/features.csv", header = TRUE)
+}
+if (cohortType == "multiple"){
+    df <- read.csv("outputs/radiomics/Multiple 3T Scanner Cohort/features.csv", header = TRUE)
+} 
+if (cohortType == "pdstate"){
+    df <- read.csv("outputs/radiomics/PD-state Cohort/features.csv", header = TRUE)
+}
 
 # Set output index as numeric value
 df[,ncol(df)] <- as.numeric(df[,ncol(df)])
+
+df = subset(df, select = -c(X))
 
 # Create dataset
 f_data <- mRMR.data(data = data.frame(df))
@@ -35,6 +47,15 @@ f_data <- mRMR.data(data = data.frame(df))
 solution <- mRMR.classic(data=f_data, target_indices=ncol(df), feature_count = as.integer(numberOfFeatures))
 
 # Get index of top 7 features
-write.csv(solutions(solution), "outputs/radiomics/featureIndex.csv")
-
-cat(green("Succesfully computed top features, index available in: outputs/radiomics/featureIndex.csv"))
+if (cohortType == "reference"){
+    write.csv(solutions(solution), "outputs/radiomics/Reference Cohort/featureIndex.csv")
+    cat(green("Succesfully computed top features, index available in: outputs/radiomics/Reference Cohort/featureIndex.csv"))
+}
+if (cohortType == "multiple"){
+    write.csv(solutions(solution), "outputs/radiomics/Multiple 3T Scanner Cohort/featureIndex.csv")
+    cat(green("Succesfully computed top features, index available in: outputs/radiomics/Multiple 3T Scanner Cohort/featureIndex.csv"))
+}
+if (cohortType == "pdstate"){
+    write.csv(solutions(solution), "outputs/radiomics/PD-state Cohort/featureIndex.csv")
+    cat(green("Succesfully computed top features, index available in: outputs/radiomics/PD-state-cohort/featureIndex.csv"))
+}
